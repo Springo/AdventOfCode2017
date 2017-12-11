5494955ac1ab89647b2e70dd483b25a1430b2d02
Kevin Xia <kevinmxia@gmail.com>
2017-12-11 00:15:56 -0500

    Day 11.

---
+++ d11.py
@@ -0,0 +1,29 @@
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line[:-1])
+    return lines
+
+lines = readFile("d11input.txt")
+steps = lines[0].split(',')
+print(steps)
+
+highest = 0
+y = 0
+x = 0
+for s in steps:
+    if s[0] == 's':
+        y -= 1
+    else:
+        y += 1
+    if s[-1] == 'e':
+        x += 1
+    elif s[-1] == 'w':
+        x -= 1
+    steps = max(abs(x), abs(y))
+    if steps > highest:
+        highest = steps
+
+print(max(abs(x),abs(y)))
+print(highest)
