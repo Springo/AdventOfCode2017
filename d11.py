7a00be7033b4d88d2a6d9fcd552849a3732add4b
Kevin Xia <kevinmxia@gmail.com>
2017-12-11 20:14:59 -0500

    Update day 11 code to work correctly.

--- d11.py
+++ d11.py
@@ -15,15 +15,23 @@ x = 0
 for s in steps:
     if s[0] == 's':
         y -= 1
+        if s[-1] == 'e':
+            x += 1
+        elif s[-1] == 'w':
+            x -= 1
+        else:
+            y -= 1
     else:
         y += 1
-    if s[-1] == 'e':
-        x += 1
-    elif s[-1] == 'w':
-        x -= 1
-    stps = max(abs(x), abs(y))
+        if s[-1] == 'e':
+            x += 1
+        elif s[-1] == 'w':
+            x -= 1
+        else:
+            y += 1
+    stps = max(abs(x), (abs(y) - abs(x)) // 2 + abs(x))
     if stps > highest:
         highest = stps
 
-print(max(abs(x),abs(y)))
+print(max(abs(x), (abs(y) - abs(x)) // 2 + abs(x)))
 print(highest)
